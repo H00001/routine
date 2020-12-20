b47324db6ae5c13962a6149127afb5549ac7ada8
frank <system-noreply@gunplan.top>
2020-12-20 13:49:38 +0800

    Refactor[routine.c]: Add cmake file support[#29]beta 5

--- src/routine_user_api.h
+++ src/routine_user_api.h
@@ -8,11 +8,9 @@
 #include "routine_base.h"
 #include <stdarg.h>
 
-typedef struct __u_routine {
-    rid_t rid;
-    rid_t pid;
+typedef struct u_routine {
+    info i;
     data_t consequence;
-    STATUS status;
 } uroutine;
 
 extern void exchange();
@@ -21,27 +19,27 @@ rid_t create_routine(any p);
 
 rid_t create_routine_with_params(any p, int num, ...);
 
-static int user_cond(rid_t rid, enum _EVENT e);
+static unsigned int user_cond(rid_t rid, enum _EVENT e);
 
-void insert_uroutine_map(rid_t id, rid_t pid, data_t p, STATUS s);
+void insert_uroutine_map(info ii, data_t p);
 
-static void execute_complete(rid_t id, rid_t pid, data_t r, STATUS s);
+static void execute_complete(info i, data_t r);
 
 /**  user level 0 api **/
 
-const rid_t get_pid(rid_t);
+rid_t get_pid(rid_t);
 
-const rid_t get_rid();
+rid_t get_rid();
 
-const int block(rid_t rid);
+unsigned int block(rid_t rid);
 
-const int resume(rid_t rid);
+unsigned int resume(rid_t rid);
 
-const int wait_all();
+int wait_all();
 
-const STATUS get_status(rid_t rid);
+STATUS get_status(rid_t rid);
 
-const data_t get_consequence(rid_t rid);
+data_t get_consequence(rid_t rid);
 
 /**
  * wait routine, current routine will wait it's child routine
@@ -49,6 +47,6 @@ const data_t get_consequence(rid_t rid);
  * @param id child routine id
  * @return wait times. times of executing function of `exchange`
  */
-const int wait_rt(const rid_t id);
+int wait_rt(rid_t id);
 
 #endif //ROUTINE_ROUTINE_USER_API_H
